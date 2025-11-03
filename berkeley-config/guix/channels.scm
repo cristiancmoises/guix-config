987a5893f4203bc5800d69a04c5d8f9f0f404681
Berkeley <ethicalhacker@riseup.net>
2025-11-03 06:08:49 -0300

    Update: New channel

--- berkeley-config/guix/channels.scm
+++ berkeley-config/guix/channels.scm
@@ -16,17 +16,23 @@
  ;    "9edb3f66fd807b096b48283debdcddccfea34bad"
  ;    (openpgp-fingerprint
  ;     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-(channel
-       (name 'guix)
-       (url "https://codeberg.org/guix/guix.git")
-       (branch "master")
-       (commit "4fd32c00abb59b0f275a93c3fef35d5193adbe7a")
-       (introduction
-        (make-channel-introduction
-         "9edb3f66fd807b096b48283debdcddccfea34bad"
-         (openpgp-fingerprint
-          "BBB0 2DDF 2CEA F6A8 0D1D E643 A2A0 6DF2 A33A 54FA"))))
-      (channel
+ ;(channel
+ ;      (name 'guix)
+ ;      (url "https://codeberg.org/guix/guix.git")
+ ;      (branch "master")
+ ;      (commit "4fd32c00abb59b0f275a93c3fef35d5193adbe7a")
+ ;      (introduction
+ ;       (make-channel-introduction
+ ;        "9edb3f66fd807b096b48283debdcddccfea34bad"
+ ;        (openpgp-fingerprint
+ ;         "BBB0 2DDF 2CEA F6A8 0D1D E643 A2A0 6DF2 A33A 54FA"))))
+ 
+ (channel
+    (name 'debdistutils)
+    (url "https://gitlab.com/debdistutils/guix/mirror.git")
+    (branch "master"))
+
+     (channel
        (name 'guix-north-america)
        (url "https://git.genenetwork.org/guix-north-america/")
        (branch "master")
--- channels.scm
+++ channels.scm
@@ -2,21 +2,46 @@
 
 ;; Define the list of channels
 (list
-  (channel-with-substitutes-available
-   %default-guix-channel
-   "https://ci.guix.gnu.org")
+;  (channel-with-substitutes-available
+ ;  %default-guix-channel
+ ;  "https://ci.guix.gnu.org")
   
-  (channel
-   (name 'guix)
-   (url "https://git.savannah.gnu.org/git/guix.git")
-   (branch "master")
-   (commit "17119f1a35b7bff683f2f8f2b5db09b4634283e6")
-   (introduction
-    (make-channel-introduction
-     "9edb3f66fd807b096b48283debdcddccfea34bad"
-     (openpgp-fingerprint
-      "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-  
+ ; (channel
+ ;  (name 'guix)
+ ;  (url "https://git.savannah.gnu.org/git/guix.git")
+ ;  (branch "master")
+ ;  (commit "17119f1a35b7bff683f2f8f2b5db09b4634283e6")
+ ;  (introduction
+ ;   (make-channel-introduction
+ ;    "9edb3f66fd807b096b48283debdcddccfea34bad"
+ ;    (openpgp-fingerprint
+ ;     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+ ;(channel
+ ;      (name 'guix)
+ ;      (url "https://codeberg.org/guix/guix.git")
+ ;      (branch "master")
+ ;      (commit "4fd32c00abb59b0f275a93c3fef35d5193adbe7a")
+ ;      (introduction
+ ;       (make-channel-introduction
+ ;        "9edb3f66fd807b096b48283debdcddccfea34bad"
+ ;        (openpgp-fingerprint
+ ;         "BBB0 2DDF 2CEA F6A8 0D1D E643 A2A0 6DF2 A33A 54FA"))))
+ 
+ (channel
+    (name 'debdistutils)
+    (url "https://gitlab.com/debdistutils/guix/mirror.git")
+    (branch "master"))
+
+     (channel
+       (name 'guix-north-america)
+       (url "https://git.genenetwork.org/guix-north-america/")
+       (branch "master")
+       (introduction
+        (make-channel-introduction
+         "c0979ad86fdf0b403c60d5767328cb862ecc00ef"
+         (openpgp-fingerprint
+          "F8D5 46F3 AF37 EF53 D1B6 48BE 7B4D EB93 212B 3022"))))
+
   (channel
    (name 'nonguix)
    (url "https://gitlab.com/nonguix/nonguix")
