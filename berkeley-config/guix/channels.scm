911152a1f9a3442cd7f5a43e411e257e4e1f99bd
Berkeley <ethicalhacker@riseup.net>
2025-11-03 18:41:08 -0300

    Update: Installer Script

--- berkeley-config/guix/channels.scm
+++ berkeley-config/guix/channels.scm
@@ -42,6 +42,17 @@
          (openpgp-fingerprint
           "F8D5 46F3 AF37 EF53 D1B6 48BE 7B4D EB93 212B 3022"))))
 
+(channel
+       (name 'guix)
+       (url "https://codeberg.org/guix/guix.git")
+       (branch "master")
+       (commit "4fd32c00abb59b0f275a93c3fef35d5193adbe7a")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D E643 A2A0 6DF2 A33A 54FA"))))
+
   (channel
    (name 'nonguix)
    (url "https://gitlab.com/nonguix/nonguix")
---
+++ installer.sh
@@ -0,0 +1,56 @@
+
+#!/bin/bash
+
+# Display banner
+echo "+-------------------------------------------------------------+"
+echo "|                  GNU GUIX INSTALLATION                                  |"
+echo "|                      [  In Code We Trust  ]                                         |"
+echo "+-------------------------------------------------------------+"
+echo ""
+
+CONFIG_FILE="/mnt/config.scm"
+
+# Ensure the file exists
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "Error: $CONFIG_FILE does not exist."
+  exit 1
+fi
+
+# Insert (nongnu packages linux) and (nongnu packages firmware) into (use-modules ...)
+sed -i '/(use-modules (gnu)/ {
+  /nongnu packages linux/! s/)/ (nongnu packages linux)/
+}' "$CONFIG_FILE"
+
+sed -i '/(use-modules (gnu)/ {
+  /nongnu packages firmware/! s/)/ (nongnu packages firmware)/
+}' "$CONFIG_FILE"
+
+# Insert (kernel linux) and (firmware (list linux-firmware)) into (operating-system ...)
+sed -i '/(operating-system/ {
+  N
+  /kernel linux/! s/)/ (kernel linux)/
+}' "$CONFIG_FILE"
+
+sed -i '/(operating-system/ {
+  N
+  /firmware (list linux-firmware)/! s/)/ (firmware (list linux-firmware))/
+}' "$CONFIG_FILE"
+
+# Start cow-store
+echo "[+] Starting cow-store service..."
+herd start cow-store /mnt
+
+# Download and move channels.scm
+echo "[+] Downloading channels.scm..."
+wget https://short.securityops.co/channels -O /mnt/etc/channels.scm
+
+# Make it writable
+chmod +w /mnt/etc/channels.scm
+
+# Reconfigure system
+echo "[+] Reconfiguring system with Guix..."
+sudo guix system reconfigure /mnt/etc/config.scm
+
+# Reboot
+echo "[+] Rebooting system now..."
+reboot
