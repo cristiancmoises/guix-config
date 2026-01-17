39df5b5c2fca908bc523d5ee19146743821956bd
Berkeley <ethicalhacker@riseup.net>
2026-01-17 07:46:19 -0300

    Updated: Kernel

--- berkeley-config/etc/config.scm
+++ berkeley-config/etc/config.scm
@@ -228,12 +228,12 @@
   (package
     (inherit linux)
     (name "securityops")
-    (version "6.18.4") 
+    (version "6.18.5") 
     (source (origin
               (method url-fetch)
-              (uri "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.18.4.tar.xz") ;;  :)
+              (uri "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.18.5.tar.xz") ;;  :)
               (sha256
-               (base32 "1asza9m4vb7lghxaiy5fpnbwmb9a44pgjclbpgv1p77plnf16l7q"))))
+               (base32 "0h6r5w7755gd8ys18zi9rgw95wrd2yalbq0h88ihv3ggki01z78q"))))
     (arguments
      (substitute-keyword-arguments (package-arguments linux)
        ((#:defconfig _) (list (local-file "/etc/securityops.defconfig")))
@@ -777,7 +777,7 @@ table inet filter {
         udp dport 51820 accept
 
         # Mullvad control servers (TCP 443)
-        ip saddr {$MULLVAD} tcp sport 443 ct state established accept
+        ip saddr {$MULLVADIP} tcp sport 443 ct state established accept
 
         # Tor daemon local ports
         tcp dport {9050,9040,5353} iif \"lo\" accept
@@ -816,7 +816,7 @@ table inet filter {
         oif \"wg0-mullvad\" { udp dport 53, tcp dport 53 } ip daddr 100.64.0.23 accept
 
         # Mullvad control servers
-        ip daddr {$MULLVAD} tcp dport 443 accept
+        ip daddr {$MULLVADIP} tcp dport 443 accept
 
         # Tor local ports
         oif \"lo\" tcp dport {9050,9040,5353} accept
